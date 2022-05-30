9334f19f3e4edefe7cbd2b559b7eb1cc765ce1e2
Balamurugan011 <balamuruganiyappanbm@gmail.com>
2022-05-30 22:57:23 +0530

    First Commit

---
+++ src/org/base/ClassA.java
@@ -0,0 +1,25 @@
+package org.base;
+
+public class ClassA {
+	private void test1() {
+		System.out.println("Test1");
+	}
+	private void test2() {
+		System.out.println("Test2");
+	}
+	private void test3() {
+		System.out.println("Test3");
+	}
+	private void test4() {
+		System.out.println("Test4");
+	}
+	private void test5() {
+		System.out.println("Test5");
+	}
+	private void test6() {
+		System.out.println("Test6");
+	}
+	
+
+
+}
